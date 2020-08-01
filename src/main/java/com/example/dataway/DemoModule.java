766e5e52d977ba6745e2c117d26ac8ce35caa44b
LiXiaoJiang <lizhenjiang.xidian@foxmail.com>
2020-08-01 13:58:37 +0800

    integrated Dataway for the first time

---
+++ src/main/java/com/example/dataway/DemoModule.java
@@ -0,0 +1,4 @@
+package com.example.dataway;
+
+public class DemoModule {
+}
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,21 +1,4 @@
 package com.example.demo.controller;
 
-import com.example.demo.domain.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-@RestController
-@RequestMapping("/users")
 public class UserController {
-    //创建map
-    static Map<Long, User> userMap = new HashMap<>();
-    //创建线程安全的map
-//    static Map<Long, User> userMap = Collections.synchronizedMap(new HashMap<Long, User>());
-
-
-
 }
