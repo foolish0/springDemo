790a7f294c4c7e5084efca7a0963a964ceceb153
Gabriel <lizhenjiang.xidian@foxmail.com>
2021-08-04 00:18:32 +0800

    Gabriel - 自定义类加载器加载Hello.java

---
+++ src/main/java/com/example/demo/training/MyClassLoader.java
@@ -0,0 +1,30 @@
+package com.example.demo.training;
+
+
+import java.math.BigDecimal;
+import java.util.Base64;
+
+public class MyClassLoader extends ClassLoader{
+    public static void main(String[] args) {
+        try {
+            new MyClassLoader().findClass("training.Hello").newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        String helloBase64 = "yv66vgAAADQAHAoABgAOCQAPABAIABEKABIAEwcAFAcAFQEABjxpbml0PgEAAygpVgEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBAAg8Y2xpbml0PgEAClNvdXJjZUZpbGUBAApIZWxsby5qYXZhDAAHAAgHABYMABcAGAEAFEhlbGxvIE15Q2xhc3NMb2FkZXIhBwAZDAAaABsBAA50cmFpbmluZy9IZWxsbwEAEGphdmEvbGFuZy9PYmplY3QBABBqYXZhL2xhbmcvU3lzdGVtAQADb3V0AQAVTGphdmEvaW8vUHJpbnRTdHJlYW07AQATamF2YS9pby9QcmludFN0cmVhbQEAB3ByaW50bG4BABUoTGphdmEvbGFuZy9TdHJpbmc7KVYAIQAFAAYAAAAAAAIAAQAHAAgAAQAJAAAAHQABAAEAAAAFKrcAAbEAAAABAAoAAAAGAAEAAAADAAgACwAIAAEACQAAACUAAgAAAAAACbIAAhIDtgAEsQAAAAEACgAAAAoAAgAAAAUACAAGAAEADAAAAAIADQ==";
+        byte[] bytes = decode(helloBase64);
+        return defineClass(name, bytes, 0, bytes.length);
+    }
+
+    private byte[] decode(String base64) {
+        return Base64.getDecoder().decode(base64);
+    }
+}
